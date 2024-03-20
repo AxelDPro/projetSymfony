d879c811140cf3e669a17ea758b0f9d4fc1b68ef
YeroTPro <thecross579@gmail.com>
2024-03-20 16:28:14 +0100

    Yéro - BienController et VueBienHtmlTwig

--- public/index.php
+++ public/index.php
@@ -2,8 +2,8 @@
 
 use App\Kernel;
 
-<link rel="stylesheet" href="css/bootstrap.css">
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+/* <link rel="stylesheet" href="css/bootstrap.css">
+ */require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
