cc25415d82899a64ea507a373be9e35bf7216bde
AxelDPro <a.desmaretpro@gmail.com>
2024-03-20 16:06:40 +0100

    Axel : Création Controller

--- public/index.php
+++ public/index.php
@@ -2,9 +2,11 @@
 
 use App\Kernel;
 
-<link rel="stylesheet" href="css/bootstrap.css">
+// <link rel="stylesheet" href="css/bootstrap.css"> Cette ligne a été commentée car elle ne fait pas partie du code PHP
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
