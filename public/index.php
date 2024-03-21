a53f10f59387f5304b10a06abc124c49b5952c4a
oussamaaapro <oussamamoukas64@gmail.com>
2024-03-21 14:03:16 +0100

    oussama: menuV1

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 use App\Kernel;
 
-// <link rel="stylesheet" href="css/bootstrap.css"> Cette ligne a été commentée car elle ne fait pas partie du code PHP
+
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
