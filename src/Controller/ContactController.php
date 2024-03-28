0aaa9e79548b4813b96b9faaad5e18d8271db4e7
oussamaaapro <oussamamoukas64@gmail.com>
2024-03-28 15:53:44 +0100

    Oussama: mailtrap

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,11 +10,13 @@ use App\Form\ContactType;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Contact;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(ContactRepository $contactRepository, Request $request, EntityManagerInterface $manager): Response
+    public function index(ContactRepository $contactRepository, Request $request, EntityManagerInterface $manager,MailerInterface $mailer): Response
     {
         $contact = new Contact();
         $form=$this->createForm(ContactType::class,$contact);
@@ -23,6 +25,21 @@ class ContactController extends AbstractController
         if($form->isSubmitted() && $form->isValid()){
             $manager->persist($contact);
             $manager->flush();
+
+            $email = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+
             return $this->redirectToRoute("app_contact");
         }
         return $this->render('contact/index.html.twig', [
